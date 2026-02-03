d9b6b9a6fd2a06e1136b953545e12ccc3df5a09b
Nonbell18 <benjamin.karlsson3@learnet.se>
2026-02-03 14:44:08 +0100

    working on spawning obstacles

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 let runner = document.getElementById("runner");
+let obstacle = document.querySelector(".obstacle");
+let obstacles = [];
 let isJumping = false;
+
+//Game running
+function runGame(event) {
+  if (event.code === "Space") {
+    console.log("Space Pressed!");
+
+    let duration = 10000 * Math.random();
+
+    for (let i = 0; i < 3; i++) {
+      let clone = obstacle.cloneNode(true);
+      clone.id = "obstacle" + i;
+      obstacles.push(clone);
+      document.getElementById("game").appendChild(clone);
+      /** @type {Animation} */
+      let animation = clone.animate(
+        [
+          { transform: "translateX(100vw)" }, // Startläge
+          { transform: "translateX(-100vw)" }, // Slutläge
+        ],
+        {
+          duration: duration, // Tid i millisekunder
+          delay: 1000 * i, // Vänta lite innan start
+        }
+      );
+      animation.onfinish = () => {
+        console.log(obstacles);
+        clone.remove();
+        obstacles = obstacles.filter((h) => h !== clone);
+      };
+    }
+  }
+}
+
+/**
+ *
+ * @param {KeyboardEvent} event
+ */
 
 //jumping function
 function jump(event) {
@@ -7,11 +46,13 @@ function jump(event) {
     isJumping = true;
     runner.classList.add("jump");
 
+    //jumps with debounce
     setTimeout(() => {
       runner.classList.remove("jump");
       isJumping = false;
-    }, 1000);
+    }, 550);
   }
 }
 
 document.addEventListener("keydown", jump);
+document.addEventListener("keydown", runGame, { once: true });
